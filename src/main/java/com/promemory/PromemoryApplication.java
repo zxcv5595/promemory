91fa228347cae977c81a187d538f0c364564aea3
zxcv5595 <zxcv5595@korea.ac.kr>
2023-09-17 01:42:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/promemory/PromemoryApplication.java
@@ -0,0 +1,13 @@
+package com.promemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/promemory/PromemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
