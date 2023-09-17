a3b1dd411be124c8e396046b81de05fded71b452
zxcv5595 <zxcv5595@korea.ac.kr>
2023-09-17 12:01:17 +0900

    배포 테스트 02

--- src/main/java/com/promemory/swagger/config/SwaggerConfig.java
+++ src/main/java/com/promemory/swagger/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("Promemory API docs 001")
+                .title("Promemory API docs")
                 .description("비사이드 포텐데이 309 promemory")
                 .version("v1.0.0");
 
