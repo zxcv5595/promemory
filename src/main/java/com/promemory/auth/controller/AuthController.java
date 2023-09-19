204102f3fc6c8dad3b22d1817f432cde72a43712
zxcv5595 <zxcv5595@korea.ac.kr>
2023-09-20 02:37:10 +0900

    swagger 문서 summary, description 작성

--- src/main/java/com/promemory/auth/controller/AuthController.java
+++ src/main/java/com/promemory/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.promemory.auth.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.promemory.auth.openfeign.dto.KakaoProfile;
 import com.promemory.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,15 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "카카오 로그인",
+            description = """
+                    카카오로부터 code와 access token을 받아 사용자 정보를 받아오고, 이 정보로 로그인 또는 회원가입 진행
+
+                    jwt값을 리턴해주며, 회원전용 서비스를 사용할 때 사용됨
+
+                    회원이 아닌 유저는 회원가입되고 jwt값을 리턴해줌
+
+                    """)
     @GetMapping("/auth/kakao/callback")
     public String kakaoCallback(@Parameter String code) throws JsonProcessingException {
 
