c110e58f21e79a44dc0253c9993b1f785fe2543f
zxcv5595 <zxcv5595@korea.ac.kr>
2023-09-20 23:32:11 +0900

    SecurityConfig 수정 - filter를 추가 하지않아 jwt로 authenticated를 할 수 없었음

--- src/main/java/com/promemory/security/config/SecurityConfig.java
+++ src/main/java/com/promemory/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.promemory.security.config;
 
+import com.promemory.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,31 +11,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement((sessionManagement) ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(frameOptions -> headers.disable()))
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests.requestMatchers(
                                         "/v3/api-docs/**",
                                         "/swagger-ui/**",
                                         "/health",
+                                        "/error",
                                         "/index.html"
                                 ).permitAll()
                                 .requestMatchers(
                                         "/auth/kakao/callback"
                                 ).permitAll()
                                 .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
@@ -44,5 +52,6 @@ public class SecurityConfig {
             AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
 
 }
